c4f38a181d61f33431ce430d38711086a877594b
WandaSimeoli <wandasimeoli@gmail.com>
2023-09-08 19:35:26 +0200

    Add partials and layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel Comics</title>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-</head>
-<body>
-    <h1>Hello World</h1>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
-</body>
-</html>
+@extends('layout.main') 
+
+@section('main-content')
+
+@endsection
---
+++ resources/views/partials/header-import.blade.php
@@ -0,0 +1,1 @@
+<h1>Laravel Comics</h1>
