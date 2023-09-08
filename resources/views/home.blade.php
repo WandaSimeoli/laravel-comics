41860a3c2a1ccf63366488c35aa7053bd6844154
WandaSimeoli <wandasimeoli@gmail.com>
2023-09-09 01:06:51 +0200

    Foreach Array

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,14 @@
 @extends('layout.main') 
 @section('main-content')
     <h2>Homepage</h2>
-    <?php var_dump($comics);
-    ?>
+   <div>
+    @foreach ($comics as $comic)
+    <ul>
+        @foreach ($comic as $key => $value)
+        <li> {{$key}}: {{ $value}} </li>
+        @endforeach
+    </ul>
+      @endforeach
+   </div>
 @endsection
 
