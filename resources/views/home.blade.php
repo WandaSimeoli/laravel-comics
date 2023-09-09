627d974b3a18431fb5df923d97bc451391c844b0
WandaSimeoli <wandasimeoli@gmail.com>
2023-09-09 15:57:31 +0200

    Corrections

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,26 @@
 @extends('layout.main') 
 @section('main-content')
 <main class="bg-secondary">
-<h2 class="text-center p-3">Homepage</h2>
-<div class="container-sm">
-<div class="row">
+  <h2 class="text-center p-3">Homepage</h2>
+  <div class="container-sm">
+    <div class="row align-items-strech">
     @foreach ($comics as $comic)
-        <div class="col-3 mb-3">
-        <div class="card p-3 bg-black text-white border-white">
-        <h3 class="card-title"> {{ $comic['title']}} </h3>
-        <div class="card-body">
-        <img src="{{ $comic['thumb']}}" class="card-img-top" alt="">
-        <h4> Price: {{$comic['price']}} </h4>
-        <h5> Serie: {{$comic['series']}}</h5>
-        <div>Year: {{$comic['sale_date']}}</div>
-        <div>Typology: {{$comic['type']}}</div>
-        <p class="lh-sm">Description: {{$comic['description']}}</p>
+      <div class="col-3">
+        <div class="card p-3 bg-black text-white border-white h-75">
+          <h3> {{ $comic['title']}} </h3>
+          <div class="card-body h-50">
+            <img src="{{ $comic['thumb']}}" class="card-img-top" alt="">
+            <h4> Price: {{$comic['price']}} </h4>
+            <h5> Serie: {{$comic['series']}}</h5>
+            <div>Year: {{$comic['sale_date']}}</div>
+            <div>Typology: {{$comic['type']}}</div>
+            <p class="lh-sm overflow-auto h-25">Description: {{$comic['description']}}</p>
+          </div>
         </div>
-        </div>
-        </div>
-      @endforeach
-</div>
-</div>
+      </div>
+    @endforeach
+    </div>
+  </div>
 </main>
 @endsection
 
