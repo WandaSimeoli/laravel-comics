6aff0a7350c02a807ecce669f06b15272f482d70
WandaSimeoli <wandasimeoli@gmail.com>
2023-09-09 14:54:06 +0200

    Add Style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,26 @@
 @extends('layout.main') 
 @section('main-content')
-    <h2>Homepage</h2>
-   <div>
+<main class="bg-secondary">
+<h2 class="text-center p-3">Homepage</h2>
+<div class="container-sm">
+<div class="row">
     @foreach ($comics as $comic)
-    <ul>
-        @foreach ($comic as $key => $value)
-        <li> {{$key}}: {{ $value}} </li>
-        @endforeach
-    </ul>
+        <div class="col-3 mb-3">
+        <div class="card p-3 bg-black text-white border-white">
+        <h3 class="card-title"> {{ $comic['title']}} </h3>
+        <div class="card-body">
+        <img src="{{ $comic['thumb']}}" class="card-img-top" alt="">
+        <h4> Price: {{$comic['price']}} </h4>
+        <h5> Serie: {{$comic['series']}}</h5>
+        <div>Year: {{$comic['sale_date']}}</div>
+        <div>Typology: {{$comic['type']}}</div>
+        <p class="lh-sm">Description: {{$comic['description']}}</p>
+        </div>
+        </div>
+        </div>
       @endforeach
-   </div>
+</div>
+</div>
+</main>
 @endsection
 
--- resources/views/partials/header-import.blade.php
+++ resources/views/partials/header-import.blade.php
@@ -1,1 +1,1 @@
-<h1>Laravel Comics</h1>
+<h1 class="m-3">Laravel Comics</h1>
